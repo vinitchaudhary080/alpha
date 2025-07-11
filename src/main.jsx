57363e3bb909f7e9e4796d8fae0c4ba697d7fad2
vinitchaudhary080 <vinitchaudhary080@gmail.com>
2025-07-11 10:57:46 +0530

    main mia add

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<StrictMode>
+    <App />  </StrictMode> );
