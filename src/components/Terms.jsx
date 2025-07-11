0359b37444940edb30f7268d0915e0c372e7eba4
vinitchaudhary080 <vinitchaudhary080@gmail.com>
2025-07-11 10:52:21 +0530

    terms add

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Terms() {
+const Terms = () => {
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-8">
@@ -113,3 +113,4 @@ export default function Terms() {
     </div>
   );
 }
+export default Terms
